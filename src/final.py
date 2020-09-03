d698e81eceb750da9441ecab7718f2a779e20016
realKyawSwar <kyaw.s.thein@gmail.com>
2020-09-03 16:33:34 +0800

    Update final.py

--- src/final.py
+++ src/final.py
@@ -35,6 +35,13 @@ def df_for_line(df, line, days_overdue):
     return df[mask]
 
 
+def line_select(df, line):
+    temp_df = df.loc[df['Line'] == line]
+    temp_dict = temp_df.groupby("Type").Type.count().to_dict()
+    temp_dict['Line'] = line
+    return temp_dict
+
+
 if __name__ == "__main__":
     templater = DefaultTemplater("template.html", "report.html")
     df = pd.read_csv('final.csv', sep=';', index_col=False)
@@ -42,37 +49,39 @@ if __name__ == "__main__":
     df_203 = df_for_line(df, 203, -1000)
     line_df = df.groupby("Line").Line.count().to_dict()
     type_df = df.groupby("Type").Type.count().to_dict()
-    # print(type_df)
     data = pd.Series(line_df).reset_index(name='Quantity').rename(
                  columns={'index': 'Line'})
-    rows1 = list(data.to_dict("index").values())
-    columns1 = list(rows1[0].keys())
     data1 = pd.Series(type_df).reset_index(name='Quantity').rename(
                  columns={'index': 'Equipment'})
     rowy = list(data1.to_dict("index").values())
-    rows = sorted(rowy, key=lambda d: d['Quantity'], reverse=True)
-    columns = list(rows[0].keys())
-    metrics = ['Quantity']
-    dimension = ['Equipment']
-    dimension1 = ['Line']
+    pie_rows = sorted(rowy, key=lambda d: d['Quantity'], reverse=True)
+    pie_columns = list(pie_rows[0].keys())
+    pie_dimension = ['Equipment']
+    pie_metrics = ['Quantity']
+    hist_rows = [line_select(df, x) for x in [i for i in range(201, 212) if i != 206 and i != 207]]
+    # print(hist_rows)
+    hist_columns = [pie_rows[i]['Equipment'] for i in range(len(pie_rows))]
+    hist_metrics = [pie_rows[i]['Equipment'] for i in range(len(pie_rows))]
+    hist_columns.insert(0, "Line")
+    # dimension1 = [pie_rows[i]['Equipment'] for i in range(len(pie_rows))]
+    hist_stack = {'whatever': hist_metrics}
     date = "'" + datetime.now().strftime("%d %B %Y %H:%M") + "'"
     print(date)
     tags = {
         # "date": "'August 2020'",
         "date": date,
-        "metrics": metrics,
-        "dimension": dimension,
-        "rows": rows,
-        "columns": columns,
-        "metrics1": metrics,
-        "dimension1": dimension1,
-        "rows1": rows1,
-        "columns1": columns1
+        "pie_metrics": pie_metrics,
+        "pie_dimension": pie_dimension,
+        "pie_rows": pie_rows,
+        "pie_columns": pie_columns,
+        "hist_metrics": hist_metrics,
+        "hist_rows": hist_rows,
+        "hist_columns": hist_columns,
+        "hist_stack": hist_stack
     }
 
     templater.render(tags)
 
-    # with Gmail('kyaw.s.thein@gmail.com', '') as gmail:
-    #     gmail.send('joshuakyaw@outlook.com', subject='your subject',
-    #                body='your message', attachment="report.html")
-
+    # # with Gmail('kyaw.s.thein@gmail.com', '') as gmail:
+    # #     gmail.send('joshuakyaw@outlook.com', subject='your subject',
+    # #                body='your message', attachment="report.html")
